2c436cb721ca60ccaff7167cdee1b77d1cc9a3c9
Denis Reznikov <denis.reznikov@gmail.com>
2019-11-30 14:08:49 +0300

    add start comand for bot

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from telegram import Update
-from telegram.ext import Updater,CommandHandler,MessageHandler,Filters
+from telegram.ext import Updater, CommandHandler
+
 
 def do_start(update: Update, context):
-    update.message.reply_text("HI")
+    answer = " *Hello, " + update.message.from_user.first_name + "*\n I am u helper . " \
+             "if U want drink or eat tap - /eat \n" \
+             "And I will show nearest the nearest bars or cafe or restaurant"
+    update.message.reply_markdown(text=answer)
 
 
 def main():
-    updater = Updater(TOKKEN_FOR_BOT, use_context=True)
+    updater = Updater("Token", use_context=True)
     start_handler = CommandHandler("start", do_start)
     updater.dispatcher.add_handler(start_handler)
     updater.start_polling()
     updater.idle()
+
 
 if __name__ == '__main__':
     main()
