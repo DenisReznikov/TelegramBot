62d054cae16c2ebe70a85cc349d162a7658afc14
Denis Reznikov <denis.reznikov@gmail.com>
2019-12-12 18:39:18 +0300

    add environment for github and heroku

--- main.py
+++ main.py
@@ -25,7 +25,8 @@ def do_start(update: Update, context):
 
 
 def main():
-    TOKEN = os.environ.get('TOKEN_TELEGRAM')
+    TOKEN = os.environ.get('TELEGRAM_TOKEN')
+    print(TOKEN)
     updater = Updater(TOKEN, use_context=True)
     updater.dispatcher.add_handler(CommandHandler("start", do_start))
     updater.dispatcher.add_handler(eat_handler())
