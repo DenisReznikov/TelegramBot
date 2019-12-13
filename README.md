13ae9e67b57ead329e83c3ef6d27325115f6ec40
Denis Reznikov <denis.reznikov@gmail.com>
2019-12-13 14:32:27 +0300

    add logger

--- README.md
+++ README.md
@@ -2,4 +2,17 @@
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 [![BCH compliance](https://bettercodehub.com/edge/badge/DenisReznikov/TelegramPolikekBot?branch=master)](https://bettercodehub.com/)
 
-# TelegramPolikekBot
+# Telegram Пolitex bot
+
+_bot_   
+
+
+
+[@Itmo_politex_Ai_bot](https://t.me/Itmo_politex_Ai_bot) Telegram bot he
+
+
+## Requirements
+* [beautifulsoup4](https://github.com/wention/BeautifulSoup4) and [lxml](https://github.com/lxml/lxml) library for pulling data out of HTML (avito)  
+* [Python Telegram Bot](https://github.com/python-telegram-bot/python-telegram-bot) - library provides a pure Python interface for the Telegram Bot API
+* [requests](https://github.com/psf/requests) - library allows you to send HTTP requests
+
--- main.py
+++ main.py
@@ -9,25 +9,25 @@ from scr.other.logger import debug_requests
 @debug_requests
 def do_start(update: Update, context):
     # TODO: перенести клавву в другой класс
-    print("das")
     location_keyboard = [[KeyboardButton("Send location", request_location=True), ], ]
     keyboard = ReplyKeyboardMarkup(
         keyboard=location_keyboard,
         resize_keyboard=True,
     )
-    answer = " Hello, " + update.message.from_user.first_name + \
-             "\n I am u helper." \
-             " If U want drink or eat, tap on - /eat \n" \
-             "And I will show nearest the nearest bars or cafe or " \
-             "restaurant \n"\
-             "To see the weather forecast tap on - /weather" \
+
+    answer = "Привет, *" + update.message.from_user.first_name + "* " + \
+             "\n Я твой помощник." \
+             "Если ты хочешь *пить* или есть нажми -> /eat \n" \
+             " \n"\
+             "Что-бы - /weather" \
              "or if you want I can find something on the avito  tap on - /avito" \
 
     update.message.reply_text(text=answer, reply_markup=keyboard)
 
 
+
 def main():
-    TOKEN = os.environ['TELEGRAM_TOKEN']
+    TOKEN =  ['TELEGRAM_TOKEN']
     updater = Updater("790323839:AAGCpqOp4LXWd3O0DNYem32FyzF-32kRyGk", use_context=True)
     updater.dispatcher.add_handler(CommandHandler("start", do_start))
     updater.dispatcher.add_handler(eat_handler())
@@ -35,6 +35,7 @@ def main():
     updater.dispatcher.add_handler(avito_handler())
     updater.start_polling()
     updater.idle()
+
 
 
 if __name__ == '__main__':
